4dc0a791ff1097967f3c1f05eb66711b15d9ec2a
Sana-maraym90 <sanamaryam6677@gmail.com>
2024-09-13 17:56:43 +0500

    Added comments for future references

--- Assets/Scripts/RotateSpeedAuthoring.cs
+++ Assets/Scripts/RotateSpeedAuthoring.cs
@@ -25,7 +25,16 @@ public class RotateSpeedAuthoring : MonoBehaviour
 }
 
 
+
+//An entity is similar to an unmanaged lightweight GameObject, which represents specific elements of your program. However, an entity acts as an ID which associates individual unique components together, rather than containing any code or serving as a container for its associated components.
 public struct RotateSpeed : IComponentData //means it represents a component that can be attached to entities in ECS.
+//Use the IComponentData interface, which has no methods, to mark a struct as a component type. 
+//This component type can only contain unmanaged data, and they can contain methods, but it's best practice for them to just be pure data.
 {
     public float value;
 }
+
+
+//Authoring Data: This is the data and configurations you set up in the Unity Editor using MonoBehaviours and other components.
+//Baker: In ECS, a baker is a class responsible for converting the authoring data into ECS components. The baker class implements the Baker<T> interface where T is the type of authoring script. The baker converts data from the authoring script into the appropriate ECS data format.
+//Bake Process: During the baking process, the authoring data is processed and converted into ECS components, which are then added to entities. This is done when the scene is loaded or when the entities are created at runtime. The resulting ECS data is used for processing by systems.
